447f90d4bb2fca3d354b4d96a8f34ab2a9a29edb
Artem <korostylev@bolid.ru>
2024-08-27 16:31:36 +0300

    ref: почти все файлы переписаны на ts

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -25,14 +25,16 @@ import {
 } from "../../service/ingridientDetalis";
 import Modal from "../DialogModal/Modal";
 import OrderDetails from "../DialogModal/OrderDetails";
-import { Ingredient, RootState } from "../../types/type";
 const BurgerConstructor = () => {
   const [modalActive, setModalActive] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const data = useSelector((state:RootState) => state[BURGER_CONSTRUCTOR_SLICE]);
+  const data = useSelector((state) => state[BURGER_CONSTRUCTOR_SLICE]);
 
-  const user = useSelector((state:RootState) => state[USER_SLICE]);
+  const selectedIngredient = useSelector(
+    (state) => state[INGRIDIENT_DETALIS_SLICE]
+  );
+  const user = useSelector((state) => state[USER_SLICE]);
   const [loading, setLoading] = useState(false);
   const price = useMemo(() => {
     const bunPrice = data.bun ? data.bun.price * 2 : 0;
@@ -41,7 +43,7 @@ const BurgerConstructor = () => {
     );
   }, [data]);
 
-  const [, dropRef] = useDrop<Ingredient>({
+  const [{ isOver }, dropRef] = useDrop({
     accept: "burger",
     drop(item) {
       if (item.type === "bun") {
@@ -62,7 +64,7 @@ const BurgerConstructor = () => {
     }
 
     setLoading(true);
-    const success = await dispatch(createOrderApi() as any);
+    const success = await dispatch(createOrderApi());
 
     if (success) {
       setTimeout(() => {
@@ -93,7 +95,7 @@ const BurgerConstructor = () => {
         {data.ingridients && data.ingridients.length > 0 ? (
           <div className={styles["constructor__content"]}>
             {data.ingridients.map((item, index) => (
-              <IngredientCard key={item._id} item={item} index={index} />
+              <IngredientCard key={item.key} item={item} index={index} />
             ))}
           </div>
         ) : (
@@ -140,7 +142,7 @@ const BurgerConstructor = () => {
       )}
       {modalActive && (
         <Modal onClose={handleModalClose}>
-          <OrderDetails  />
+          <OrderDetails item={selectedIngredient} />
         </Modal>
       )}
     </div>
